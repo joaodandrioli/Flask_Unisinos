0dd3ebed767cb7bbe02248deaa38c3363cba01aa
joaodandrioli <144073969+joaodandrioli@users.noreply.github.com>
2025-09-01 20:31:09 -0300

    Update routes.py

--- flask/app/Hello/routes.py
+++ flask/app/Hello/routes.py
@@ -1,7 +1,13 @@
 from flask import Blueprint
 
 hello_bp = Blueprint('hello',__name__)
+nome_bp = Blueprint('Olá, João Vitor! ',__name__)
 
 @hello_bp.route('/')
 def index():
+
     return "Hello World" 
+
+@nome_bp.route('/')
+def index():
+    return "Olá, João Vitor!" 
